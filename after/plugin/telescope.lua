686670c30d03e092ba0a4093216b3180df991612
Davide Loconte <davide.loconte@hotmail.it>
2023-09-03 20:05:34 +0200

    Tweak remaps (switch to normal mode, telescope)

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,9 +1,21 @@
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function() 
+vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({search = vim.fn.input("Grep > ") });
 end)
+
+require("telescope").setup({
+    defaults = {
+        mappings = {
+            n = {
+                ["q"] = actions.close,
+            },
+        },
+    },
+})
 
 -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 -- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
